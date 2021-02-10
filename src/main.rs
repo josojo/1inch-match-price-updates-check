142589da363fe1332613ec8482d27f1ca75c81d3
josojo <josojo@hotmail.de>
2021-02-10 13:43:39 +0100

    tricking the api end points in order to prevent memorizaiton

--- src/main.rs
+++ src/main.rs
@@ -103,11 +103,17 @@ async fn main() -> Result<(), Error> {
     let mut price_change: u32 = 0;
     let mut previous_result_price = 0 as u128;
     let start = Instant::now();
-    for i in 0..50 {
-        let new_matcha_price: u128 = get_matcha_price().await.unwrap();
-        let new_1inch_price: u128 = get_1inch_price().await.unwrap();
+    for i in 0..20 {
+        let new_matcha_price: u128 = get_matcha_price(i).await.unwrap();
+        let new_1inch_price: u128 = get_1inch_price(i).await.unwrap();
+        let new_price = u128::max(new_1inch_price, new_matcha_price);
+        let price_diff = previous_result_price as f64 / new_price as f64;
         if i != 0 {
-            if previous_result_price != u128::max(new_matcha_price, new_1inch_price) {
+            if price_diff < 0.99999 as f64 || price_diff > 1.00001 as f64 {
+                println!(
+                    "New price diff of {:?} with old price of {:?} and new price of {:?}",
+                    price_diff, previous_result_price, new_price
+                );
                 price_change += 1;
             }
         }
@@ -116,7 +122,7 @@ async fn main() -> Result<(), Error> {
         // } else {
         println!("Best price from 1inch {:?}", new_1inch_price);
         // }
-        previous_result_price = u128::max(new_matcha_price, new_1inch_price).clone();
+        previous_result_price = new_price.clone();
 
         tokio::time::sleep(Duration::from_secs(5)).await;
     }
@@ -129,22 +135,22 @@ async fn main() -> Result<(), Error> {
     Ok(())
 }
 
-async fn get_matcha_price() -> Result<u128> {
+async fn get_matcha_price(i: u128) -> Result<u128> {
     let request_url = format!(
         "https://api.0x.org/swap/v1/quote?buyToken=DAI&sellToken={token}&sellAmount={sellAmount}",
         token = "WETH",
-        sellAmount = "100000000000000000" //<100 WETH>
+        sellAmount = (100000000000000000000 + i) //<100 WETH>
     );
     let response = reqwest::get(&request_url).await?;
     let result: RootMatcha = response.json().await?;
     let number = u128::from_str_radix(&result.buy_amount, 10)?;
     Ok(number)
 }
-async fn get_1inch_price() -> Result<u128> {
+async fn get_1inch_price(i: u128) -> Result<u128> {
     let request_url = format!(
         "https://api.1inch.exchange/v2.0/quote?fromTokenAddress={token}&toTokenAddress=0x6b175474e89094c44da98b954eedeac495271d0f&amount={sellAmount}",
         token = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-        sellAmount = "100000000000000000" //<100 WETH>
+        sellAmount = 100000000000000000000+i //<100 WETH>
     );
     let response = reqwest::get(&request_url).await?;
     let result: RootInch = response.json().await?;
